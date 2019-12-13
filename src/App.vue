336832d6d6c7d83ce737ff46f6678d29665fb7ae
zhangqian00 <zq_674360351@163.com>
2019-12-13 14:43:10 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<router-view/>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'App',
+		mounted(){
+			window.addEventListener('hashchange',()=>{
+				let currentPath = window.location.hash.slice(1);
+				if(this.$router.path !== currentPath){
+					this.$router.push(currentPath);
+				}
+			},false);
+		}
+	}
+</script>
+
+<style>
+	#app {
+		font-family: "Avenir", Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: left;
+		color: #2c3e50;
+		width: 1200px;
+		margin: 0 auto;
+		padding-top: 61px;
+	}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,89 @@
+<template>
+	<div id="home">
+		<canvas id='sakura'></canvas>
+		<div class="content">
+			<div class="top">
+				<router-link to='/home'>
+					<img src="@/assets/img/home/tx.jpg" alt="">
+				</router-link>
+			</div>
+			<div class="btm">
+				<router-link to='/home'>tab1</router-link>
+				<router-link to='/home'>tab2</router-link>
+				<router-link to='/home'>tab3</router-link>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Canvans from '@/utils/luohua.js'; // 落花特效
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		mounted(){
+			Canvans.luohuaCanvans(); // 落花特效
+		}
+	}
+</script>
+
+<style scoped lang='less'>
+	#home {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: -1;
+		.content {
+			padding-top: 12%;
+			position: fixed;
+			top: 0;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			.top {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				img {
+					width: 220px;
+					height: 220px;
+					border-radius: 50%;
+					animation: mylogo 3s;
+					animation-iteration-count: infinite;
+				}
+			}
+			.btm {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				a {
+					font-size: 20px;
+					padding: 10px 30px;
+					color: #409eff;
+				}
+				a:hover {
+					color: #ff8003;
+				}
+			}
+		}
+	}
+	@keyframes mylogo {
+	  0% {
+	    transform: rotate(0deg) scale(0.8, 0.8);
+	    opacity: 1;
+	  }
+	  25% {
+	    transform: rotate(0deg) scale(1, 1);
+	    opacity: 0.8;
+	  }
+	  100% {
+	    transform: rotate(0deg) scale(0.8, 0.8);
+	    opacity: 1;
+	  }
+	}
+</style>
