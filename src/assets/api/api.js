a4bb846fb5c9637d7089cdfa9a83ff96f5732784
zhangqian00 <zq_674360351@163.com>
2020-03-15 21:34:15 +0800

    配置调试，新增字体文件

--- src/assets/api/api.js
+++ src/assets/api/api.js
@@ -1,6 +1,6 @@
-const globalApi = 'http://localhost:3001';
+const globalApi = 'http://localhost:3000';
 // const globalApi = 'http://39.105.14.204:3001';
-
+// process.env.NODE_ENV
 export default {
 	blogList: globalApi + '/api/blogList', // 博客列表
 };
